107f493fe899571dac9d5d2d63c104fbc4a8cd55
Alex Zhen <suncrimson@gmail.com>
2013-08-13 20:05:39 +1000

    Initial version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Houchu::Application.config.secret_key_base = '398a04350efbddd0d1ec253cbfcbb1f6ea8f0c44a10557a72d54bc5089770b8461f90bb26d5243b96eec5886a2c3a075f15b49a88c5a00fe5ebc03f5a2a75af6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Houchu::Application.config.session_store :cookie_store, key: '_houchu_session'
